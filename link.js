ef8bf9383641af6232e366357abad589c00b9247
Kevin Dalleau <kevin.dalleau@telecomnancy.eu>
2015-06-26 17:18:14 +0200

    Write to file

--- link.js
+++ link.js
@@ -1,36 +1,52 @@
 var fs = require('fs');
 var csv = require('csv-streamify');
 var N3 = require('n3');
+var sourceFrom;
+var sourceTo;
 
 var fstream = fs.createReadStream('./raw/cco_cco_c_id_cco_pharmgkb_id.txt');
+
 var options = {
   delimiter: '\t',
   empty: '',
   columns: false
 }
+
 var parser = csv(options, getData);
+
 var writer = N3.Writer({
   prefixes: { 'biodb': 'http://biodb.jp/mappings/' }
 });
-var sourceFrom;
-var sourceTo;
+
+function writeToFile(data, fileName) {
+  fs.writeFile("./"+fileName, data, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    console.log("Fichier créé : "+fileName);
+  });
+}
+
 function getData(err, doc) {
+
   if (err) {
     console.log("Erreur lors du parcours du fichier");
   }
+
   doc.forEach(function(row, index) {
+
     if(index == 0) {
       sourceFrom = row[0];
       sourceTo = row[1];
     }
+
     else if(index == doc.length-1) {
-      console.log("On est à la fin du fichier !");
       writer.end(function (error, result) {
-        console.log(result);
+        writeToFile(result, sourceFrom+"_"+sourceTo+".ttl");
       });
     }
+
     else {
-      
       var idFrom = row[0];
       var idTo = row[1];
       writer.addTriple({
@@ -40,9 +56,7 @@ function getData(err, doc) {
       });
 
     }
-
-
-     })
+  })
 }
 
 fstream.pipe(parser);
